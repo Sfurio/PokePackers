d5cbb426c5546dd035817d8186114227793d527c
Sfurio <sfurio@gmail.com>
2024-10-14 12:25:21 -0400

    small changes

--- MyBusiness/settings.py
+++ MyBusiness/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'MyBusiness.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Make sure this points to your template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
